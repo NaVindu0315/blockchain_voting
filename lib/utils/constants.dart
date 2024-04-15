e09f3ba5b097e2d4876193bd215aaf08832cdbb5
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-15 21:22:30 +0530

    chgned

---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+String infura_url =
+    "https://sepolia.infura.io/v3/4887f9655ec94842a2d3206deae69ad2";
+
+String owner_private_key =
+    "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6";
+
+String voter_private_key =
+    "ff9f767a7acb0f87fe70a2de9321d341601c1933c89ada19768b54ed1e883f95";
+
+String contractAddress1 = "0x02F2F91EC0e1cB27044466865205D5A8116d94aF";
