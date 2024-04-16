baa5d0d95023c4b8820bc1340553c833b99e4d7e
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-16 19:39:46 +0530

    chgned paths

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blockchain/pages/home.dart';
-import 'package:flutter_blockchain/testvoting/test.dart';
-import 'package:flutter_blockchain/vote_home.dart';
+
+import 'package:flutter_blockchain/testvoting/vote_home.dart';
 
 import 'firebase_options.dart';
 
