3e991d2d288e48000398a133672e6b414d7e1bd9
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-16 20:07:07 +0530

    candidate 1 votes are ftched from smart contract

--- lib/testvoting/vote_home.dart
+++ lib/testvoting/vote_home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blockchain/testvoting/test_functions.dart';
+import 'package:http/http.dart';
+
+import 'package:web3dart/web3dart.dart';
+import 'test_constants.dart';
 
 class Vote_Home extends StatefulWidget {
   const Vote_Home({Key? key}) : super(key: key);
@@ -8,6 +13,16 @@ class Vote_Home extends StatefulWidget {
 }
 
 class _Vote_HomeState extends State<Vote_Home> {
+  Client? httpClient;
+  Web3Client? ethClient;
+
+  @override
+  void initState() {
+    httpClient = Client();
+    ethClient = Web3Client(infura_url, httpClient!);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,10 +38,21 @@ class _Vote_HomeState extends State<Vote_Home> {
                   Spacer(),
                   Column(
                     children: [
-                      Text(
-                        '0',
-                        style: TextStyle(fontSize: 30.0),
-                      ),
+                      FutureBuilder<List>(
+                          future: getvotes_1(ethClient!),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                            return Text(
+                              snapshot.data![0].toString(),
+                              style: TextStyle(
+                                  fontSize: 50, fontWeight: FontWeight.bold),
+                            );
+                          }),
                     ],
                   ),
                   Spacer(),
