1d28c3dac3ef90c0ab25653de97bbf8280df458f
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-16 09:47:14 +0530

    working now works

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,6 +5,6 @@ String owner_private_key =
     "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6";
 
 String voter_private_key =
-    "ff9f767a7acb0f87fe70a2de9321d341601c1933c89ada19768b54ed1e883f95";
+    "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6";
 
-String contractAddress1 = "0x02F2F91EC0e1cB27044466865205D5A8116d94aF";
+String contractAddress1 = "0x092fab255dc07e213cf8738f5c080ab57c608520";
