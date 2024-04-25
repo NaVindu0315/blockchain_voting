eb57f46f2be5277dab7f34a33548a3d6f67f3b9c
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-04-25 19:20:59 +0530

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,10 +1,1 @@
-String infura_url =
-    "https://sepolia.infura.io/v3/4887f9655ec94842a2d3206deae69ad2";
 
-String owner_private_key =
-    "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6";
-
-String voter_private_key =
-    "bdb3d39f69282abbce39e6d834b762a6ab093d97b94411f018b1fe607ea017e6";
-
-String contractAddress1 = "0x092fab255dc07e213cf8738f5c080ab57c608520";
